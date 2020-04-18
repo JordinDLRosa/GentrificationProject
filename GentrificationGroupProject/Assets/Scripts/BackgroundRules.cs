206a655a4bc7a39ba9ea0c26070395733675b41d
Edwin <edwin.samaniego@mail.citytech.cuny>
2020-04-17 22:48:57 -0400

    Created An Updated Bill Tracker
    Will now let you know of upcoming bills and then remove it from the list when its paid.

--- GentrificationGroupProject/Assets/Scripts/BackgroundRules.cs
+++ GentrificationGroupProject/Assets/Scripts/BackgroundRules.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 using UnityEngine.UI;
 public class BackgroundRules : MonoBehaviour {
 
+    // These will be for monitoring the day of the week
+    // Will test this later...
+    private string[] daysWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+    private int currentDayOfTheWeek = 0;
+    private int maxDayOfTheWeek = 6;
+
     // these are the time and date parts of the game
     private int currentMonth = 12;
-    private int currentDay = 26;
+    private int currentDay = 25;
     private int currentYear = 19;
     private int currentHour = 6;
     private float timeStart = 0f;
-    private int lastDay;
-    private int lastHour = 11;
+    private int lastDayOfTheMonth;
+    private int lastHourOfTheDay = 11;
 
     // these are the money parts of the game
-    private int savings = 1000;
+    private int savings = 1500;
     private bool paid = true;
 
     // this will be the part of the game that monitors your health
-    private string[] status = { "Normal", "Sick", "Hungry", "Sick & Hungry"}; 
+    private string[] status = { "Normal", "Hungry", "Sick", "Sick & Hungry" };
     private bool eaten = false;
     private int daysHungry = 0;
     private string health;
@@ -29,14 +35,23 @@ public class BackgroundRules : MonoBehaviour {
 
 
     // This will be the part of the game that monitors the upcoming bills.
-    private string[] bills = {"Rent", "Gas Bill", "Electricity Bill", "Phone Bill"};
-    private string[] dueBills = {"paid", "paid", "paid", "paid" };
-    private int[] costLiving = {600, 50, 100, 100};
-    private int paidBills = 0;
+    private string billsDisplayed = "";
+    private int totalBillsDues = 0;
+    private string[] bills = { "Rent $1300", "Gas Bill $50", "Electricity Bill $75", "Phone Bill $75" };
+    private List<string> dueBills = new List<string>();
+    private int[] livingCost = { 1300, 50, 75, 75 };
+    // cost of living for these bills is 1500.
+    // Daily food purchase will be 10 dollars so average 10*30 = 300. Total is 1800
+    // Monthly MetroCard will be 130, so 2130! You went over budget by 130 if you choose to not make food
+
+
+    // booleans for upcoming bills
     private bool rentPaid = false;
     private bool gasPaid = false;
     private bool electricityPaid = false;
     private bool cellPaid = false;
+    private bool updateBill = false;
+    private bool displayedBills = false;
 
     // These are the text objects
     public Text textDate;
@@ -50,30 +65,32 @@ public class BackgroundRules : MonoBehaviour {
     void Start() {
         health = status[1];
         stressBar = StressBarBox.GetComponent<SpriteRenderer>();
+        textBills.text = "None";
     }
 
     // Update is called once per frame
     void Update() {
-        stressBar.color = Color.white;
-        monitorDate();
+        textSavings.text = "Savings: $ " + savings.ToString();
         monitorTime();
+        monitorDate();
+        stressBarChange();
         monitorPayDay();
         monitorHealth();
         billsPaid();
-        upcomingBills();
-        textStatus.text = "Health: " + health.ToString();
-        textTime.text = "Time: " + currentHour + ":" + (Mathf.Round(timeStart) + " pm".ToString());
-        textDate.text = "Date: " + currentMonth + " / " + currentDay + " / " + currentYear.ToString();
-        textSavings.text = "Savings: $ " + savings.ToString();
+        addToBills();
+        if (updateBill) {
+            displayBills();
+            updateBill = false;
+        }
     }
-
     private void monitorTime() {
+        textTime.text = "Time: " + currentHour + ":" + (Mathf.Round(timeStart) + " pm".ToString());
         // remove this:
         float speedUp = 10;
         //
         timeStart += Time.deltaTime * speedUp;
 
-        if (currentHour >= lastHour) {
+        if (currentHour >= lastHourOfTheDay) {
             currentHour = 6;
             currentDay++;
             if (eaten != true) {
@@ -85,8 +102,8 @@ public class BackgroundRules : MonoBehaviour {
             currentHour++;
         }
     }
-
     private void monitorHealth() {
+        textStatus.text = "Status: " + health.ToString();
         if (daysHungry == 0) {
             health = status[0];
         }
@@ -99,55 +116,58 @@ public class BackgroundRules : MonoBehaviour {
             health = status[2];
         }
     }
-
     private void monitorDate() {
+        textDate.text = "Date: " + currentMonth + " / " + currentDay + " / " + currentYear.ToString();
         if (currentMonth == 01) {
-            lastDay = 31;
+            lastDayOfTheMonth = 31;
         }
-        if (currentMonth == 02) {
-            lastDay = 29;
+        if (currentMonth == 02 && currentYear % 4 == 0) {
+            lastDayOfTheMonth = 29;
+        }
+        else {
+            lastDayOfTheMonth = 28;
         }
         if (currentMonth == 03) {
-            lastDay = 31;
+            lastDayOfTheMonth = 31;
         }
         if (currentMonth == 04) {
-            lastDay = 30;
+            lastDayOfTheMonth = 30;
         }
         if (currentMonth == 05) {
-            lastDay = 31;
+            lastDayOfTheMonth = 31;
         }
         if (currentMonth == 06) {
-            lastDay = 30;
+            lastDayOfTheMonth = 30;
         }
         if (currentMonth == 07) {
-            lastDay = 31;
+            lastDayOfTheMonth = 31;
         }
         if (currentMonth == 08) {
-            lastDay = 31;
+            lastDayOfTheMonth = 31;
         }
         if (currentMonth == 09) {
-            lastDay = 30;
+            lastDayOfTheMonth = 30;
         }
         if (currentMonth == 10) {
-            lastDay = 31;
+            lastDayOfTheMonth = 31;
         }
         if (currentMonth == 11) {
-            lastDay = 30;
+            lastDayOfTheMonth = 30;
         }
         if (currentMonth == 12) {
-            lastDay = 31;
+            lastDayOfTheMonth = 31;
         }
-        if (currentDay > lastDay && currentMonth == 12) {
+        // Very Special Condition for next Year
+        if (currentDay > lastDayOfTheMonth && currentMonth == 12) {
             currentMonth = 1;
             currentDay = 1;
             currentYear++;
         }
-        if (currentDay > lastDay) {
+        if (currentDay > lastDayOfTheMonth) {
             currentMonth++;
             currentDay = 1;
         }
     }
-
     private void monitorPayDay() {
         // ensures you get a single payment
         if (currentDay == 6 || currentDay == 13 || currentDay == 20 || currentDay == 27) {
@@ -155,68 +175,120 @@ public class BackgroundRules : MonoBehaviour {
         }
         // on the 7th it should be payday all day. but you will actually be paid tomorrow
         if (currentDay % 7 == 0 && paid == false) {
-            savings += 600;
+            savings += 450;
             paid = true;
         }
     }
 
+    private void addToBills() {
+        if (currentMonth == 2 && currentDay == 25 && displayedBills == false) {
+            updateBill = true;
+            dueBills.Add(bills[0].ToString()); // adds rent to upComingBills from bills[0].
+            totalBillsDues += livingCost[0]; // adds rent to the totalBillsDue.
+
+        }
+        else if (currentMonth != 2 && currentDay == 26 && displayedBills == false) {
+            updateBill = true;
+            dueBills.Add(bills[0]); // adds rent to upComingBills from bills[0].
+            totalBillsDues += livingCost[0]; // adds rent to the totalBillsDue.
+        }
+        else {
+
+        }
+        if (currentDay == 4 && displayedBills == true) {
+            displayedBills = false;
+        }
+        if (currentDay == 5 && displayedBills == false) {
+            updateBill = true;
+            dueBills.Add(bills[1]); // adds Gas Bill to upComingBills from bills[1].
+            totalBillsDues += livingCost[1]; // adds Gas Bill to the totalBillsDue.
+        }
+        if (currentDay == 9 && displayedBills == true) {
+            displayedBills = false;
+        }
+        if (currentDay == 10 && displayedBills == false) {
+            updateBill = true;
+            dueBills[2] = bills[2]; // adds Electricity Bill to upComingBills from bills[2].
+            totalBillsDues += livingCost[2]; // adds Electricity Bill to the totalBillsDue.
+        }
+        if (currentDay == 15 && displayedBills == true) {
+            displayedBills = false;
+        }
+        if (currentDay == 16 && displayedBills == false) {
+            updateBill = true;
+            dueBills[3] = bills[3]; // adds Phone Bill to upComingBills from bills[3].
+            totalBillsDues += livingCost[3]; // adds Phone Bill to the totalBillsDue.
+        }
+    }
     private void billsPaid() {
-        if (currentDay == 2) {
+        if (currentDay < 2) {
             rentPaid = false;
             gasPaid = false;
             electricityPaid = false;
             cellPaid = false;
         }
-        if(currentDay == 28) {
-        }
+        // Forcefully Takes money from your savings account.
 
-        if(currentDay >= 3 && rentPaid == false) {
-            savings -= 600;
+        // Last day to Pay your Rent.
+        if (currentDay == 27 && rentPaid == false) {
+            savings -= 1300;
             rentPaid = true;
+            billsDisplayed = billsDisplayed.Replace(bills[0],"");
+            dueBills.Remove(bills[0]);
+            totalBillsDues -= livingCost[0];
+            updateBill = true;
         }
-        if (currentDay >= 12 && gasPaid == false) {
-            savings -= costLiving[1];
+        // Last Day to Pay Your Gas Bill, unless we add the ability to manually pay
+        // If so will have to code so that if you don't pay this bill by this date.
+        // The price will go up by $penalty fee everyday ?
+        if (currentDay == 12 && gasPaid == false) {
+            savings -= livingCost[1];
             gasPaid = true;
+            billsDisplayed = billsDisplayed.Replace(bills[1], "");
+            totalBillsDues -= livingCost[1];
+            updateBill = true;
         }
-        if (currentDay >= 17 && electricityPaid == false) {
-            savings -= costLiving[2];
+        // Last Day to Pay Your Electricity Bill, unless we add the ability to manually pay
+        // If so will have to code so that if you don't pay this bill by this date.
+        // The price will go up by $penalty fee everyday ?
+        if (currentDay == 17 && electricityPaid == false) {
+            savings -= livingCost[2];
             electricityPaid = true;
+            billsDisplayed = billsDisplayed.Replace(bills[2], "");
+            totalBillsDues -= livingCost[2];
+            updateBill = true;
         }
-        if (currentDay >= 23 && cellPaid == false) {
-            savings -= costLiving[3];
+        // Last Day to Pay Your Cell Bill, unless we add the ability to manually pay
+        // If so will have to code so that if you don't pay this bill by this date.
+        // The price will go up by $penalty fee everyday ?
+        if (currentDay == 23 && cellPaid == false) {
+            savings -= livingCost[3];
             cellPaid = true;
+            billsDisplayed = billsDisplayed.Replace(bills[3], "");
+            totalBillsDues -= livingCost[3];
+            updateBill = true;
         }
-
+    }
+    private void displayBills() {
+        foreach (string msg in dueBills) {
+            billsDisplayed = billsDisplayed + msg.ToString() + "\n";
+        }
+        textBills.text = billsDisplayed + "\n" + "Total Due: " + totalBillsDues.ToString();
+        displayedBills = true;
+    }
+    private void lifeSucks() {
 
     }
-
-    private void upcomingBills() {
-        if (currentDay == 27) {
-            dueBills[0] = bills[0]; // changes dueBills[0] to bills[0] and marks it as rent.
+    private void stressBarChange() {
+        if (health == "Normal") {
+            stressBar.color = Color.white;
         }
-        if (currentDay == 27) {
-            dueBills[1] = bills[1]; // changes dueBills[1] to bills[1] and marks it as Gas Bill.
+        if (health == "Hungry") {
+            stressBar.color = Color.yellow;
         }
-        if (currentDay == 10) {
-            dueBills[2] = bills[2]; // changes dueBills[2] to bills[2] and marks it as Electricity Bill.
+        if (health == "Sick") {
+            stressBar.color = Color.green;
         }
-        if (currentDay == 23) {
-            dueBills[3] = bills[3]; // changes dueBills[3] to bills[3] and marks it as Phone Bill.
-        }
-        if (dueBills.Length > 0) {
-            for(int i = 0; i < dueBills.Length; i++) {
-                if (paidBills == 4) {
-                    textBills.text = "Upcoming Bills: None".ToString();
-
-                }
-                if (dueBills[i] == "paid") {
-                    paidBills++;
-                }
-                if (dueBills[i] != "paid") {
-                    textBills.text = "Upcoming Bills: " + dueBills[i] + "\r\n".ToString();
-                }
-            }
-        }        
     }
 
 } // end of class
